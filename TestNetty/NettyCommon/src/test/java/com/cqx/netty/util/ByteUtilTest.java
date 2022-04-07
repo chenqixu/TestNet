2431715797a47ca4b286addfe9c4031d5c040d59
chenqixu <13509323824@139.com>
2022-04-07 22:27:48 +0800

    [NettyCommon]通过掩码计算子网

--- TestNetty/NettyCommon/src/test/java/com/cqx/netty/util/ByteUtilTest.java
+++ TestNetty/NettyCommon/src/test/java/com/cqx/netty/util/ByteUtilTest.java
@@ -1,11 +1,15 @@
 package com.cqx.netty.util;
 
 import org.junit.Test;
+import sun.net.util.IPAddressUtil;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.math.BigInteger;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.nio.ByteBuffer;
+import java.util.BitSet;
 import java.util.Random;
 
 public class ByteUtilTest {
@@ -100,6 +104,11 @@ public class ByteUtilTest {
         return result;
     }
 
+    public static String unsignedByte(byte b) {
+        byte[] unsignedInt1 = {0x00, b};
+        return parserUnsigned(unsignedInt1);
+    }
+
     public static String parserUnsigned(byte[] bytes) {
         return new BigInteger(bytes).toString();
     }
@@ -115,6 +124,116 @@ public class ByteUtilTest {
         System.arraycopy(b1, 0, n1, 0, b1.length);
         System.arraycopy(b2, 0, n1, b1.length, b2Len);
         return n1;
+    }
+
+    /**
+     * 功能：判断一个IP是不是在一个网段下的
+     * 格式：isInRange("192.168.8.3", "192.168.9.10/22");
+     *
+     * @param ip
+     * @param cidr
+     * @return
+     */
+    public static boolean isInRange(String ip, String cidr) {
+        String[] ips = ip.split("\\.");
+        int ipAddr = (Integer.parseInt(ips[0]) << 24)
+                | (Integer.parseInt(ips[1]) << 16)
+                | (Integer.parseInt(ips[2]) << 8)
+                | Integer.parseInt(ips[3]);
+        int type = Integer.parseInt(cidr.replaceAll(".*/", ""));
+        int mask = 0xFFFFFFFF << (32 - type);
+        String cidrIp = cidr.replaceAll("/.*", "");
+        String[] cidrIps = cidrIp.split("\\.");
+        int cidrIpAddr = (Integer.parseInt(cidrIps[0]) << 24)
+                | (Integer.parseInt(cidrIps[1]) << 16)
+                | (Integer.parseInt(cidrIps[2]) << 8)
+                | Integer.parseInt(cidrIps[3]);
+        return (ipAddr & mask) == (cidrIpAddr & mask);
+    }
+
+    /**
+     * 功能：判断一个IP是不是在一个网段下的</br>
+     * 格式：isInRange("192.168.8.3", "192.168.9.10", 22);
+     *
+     * @param srcIp         数据里的IP
+     * @param conditionIp   查询条件IP
+     * @param conditionMask 查询条件IP的掩码位数
+     * @return
+     * @throws UnknownHostException
+     */
+    public static boolean isInRange(String srcIp, String conditionIp, int conditionMask) throws UnknownHostException {
+        // 先判断是IPv4还是IPv6
+        boolean isIPv4 = IPAddressUtil.isIPv4LiteralAddress(srcIp);
+        boolean isIPv6 = IPAddressUtil.isIPv6LiteralAddress(srcIp);
+        System.out.println(String.format("isIPv4：%s，isIPv6：%s", isIPv4, isIPv6));
+        if (isIPv4) {
+            InetAddress query_host = InetAddress.getByName(srcIp);
+            InetAddress source_host = InetAddress.getByName(conditionIp);
+            // 生成掩码
+            int mask = 0xFFFFFFFF << (32 - conditionMask);
+            int query = new BigInteger(query_host.getAddress()).intValue();
+            int source = new BigInteger(source_host.getAddress()).intValue();
+            // 判断和掩码与操作后的两个子网结果是否一致
+            return (query & mask) == (source & mask);
+        } else if (isIPv6) {
+            InetAddress query_host = InetAddress.getByName(srcIp);
+            InetAddress source_host = InetAddress.getByName(conditionIp);
+            // 生成BitSet
+            BitSet qbs = bytesToBitSet(query_host.getAddress());
+            BitSet sbs = bytesToBitSet(source_host.getAddress());
+            // 生成掩码
+            BitSet mask = new BitSet();
+            for (int i = 0; i < conditionMask; i++) {
+                mask.set(i, true);
+            }
+            qbs.and(mask);// 和掩码做与操作
+            sbs.and(mask);// 和掩码做与操作
+            // 判断和掩码与操作后的两个子网结果是否一致
+            return qbs.equals(sbs);
+        }
+        return false;
+    }
+
+    public static byte[] bitSet2ByteArray(BitSet bitSet) {
+        byte[] bytes = new byte[bitSet.size() / 8];
+        for (int i = 0; i < bitSet.size(); i++) {
+            int index = i / 8;
+            int offset = 7 - i % 8;
+            bytes[index] |= (bitSet.get(i) ? 1 : 0) << offset;
+        }
+        return bytes;
+    }
+
+    public static BitSet byteArray2BitSet(byte[] bytes) {
+        BitSet bitSet = new BitSet(bytes.length * 8);
+        int index = 0;
+        for (int i = 0; i < bytes.length; i++) {
+            for (int j = 7; j >= 0; j--) {
+                bitSet.set(index++, (bytes[i] & (1 << j)) >> j == 1 ? true : false);
+            }
+        }
+        return bitSet;
+    }
+
+    /**
+     * byte数组转BitSet
+     *
+     * @param bytes byte数组
+     * @return
+     */
+    public static BitSet bytesToBitSet(byte[] bytes) {
+        BitSet bs = new BitSet(bytes.length * 8);
+        for (int i = 0; i < bytes.length; i++) {
+            bs.set(i * 8, ((byte) ((bytes[i] >> 7) & 0x1)) == 1);
+            bs.set(i * 8 + 1, ((byte) ((bytes[i] >> 6) & 0x1)) == 1);
+            bs.set(i * 8 + 2, ((byte) ((bytes[i] >> 5) & 0x1)) == 1);
+            bs.set(i * 8 + 3, ((byte) ((bytes[i] >> 4) & 0x1)) == 1);
+            bs.set(i * 8 + 4, ((byte) ((bytes[i] >> 3) & 0x1)) == 1);
+            bs.set(i * 8 + 5, ((byte) ((bytes[i] >> 2) & 0x1)) == 1);
+            bs.set(i * 8 + 6, ((byte) ((bytes[i] >> 1) & 0x1)) == 1);
+            bs.set(i * 8 + 7, ((byte) ((bytes[i] >> 0) & 0x1)) == 1);
+        }
+        return bs;
     }
 
     @Test
@@ -221,6 +340,92 @@ public class ByteUtilTest {
                 long end = System.currentTimeMillis();
                 System.out.println(String.format("cost : %s ms", end - start));
             }
+        }
+    }
+
+    @Test
+    public void ipv4Mask() throws UnknownHostException {
+        // B类网段：255.255.0.0
+        // 网络号，主机号
+        // 主机号：00000000 00000000
+        // 网络号，子网号，主机号
+        // 子网号两位
+        // 00 01 10 11 = 2的2次方-2 = 2
+        // 需要除去全0和全1的情况
+        // 子网号三位
+        // 000 001 010 011 100 101 110 111 = 2的3次方-2 = 6
+        String query_ip = "224.168.1.1";
+        int query_ip_mask = 8;
+        String source_ip = "224.179.3.0";
+        InetAddress query_host = InetAddress.getByName(query_ip);
+        InetAddress source_host = InetAddress.getByName(source_ip);
+        int mask = 0xFFFFFFFF << (32 - query_ip_mask);
+        System.out.println(Integer.toBinaryString(mask));
+        for (byte b : query_host.getAddress()) {
+            System.out.println(unsignedByte(b));
+        }
+        int query = new BigInteger(query_host.getAddress()).intValue();
+        int source = new BigInteger(source_host.getAddress()).intValue();
+        System.out.println((query & mask) == (source & mask));
+        System.out.println(isInRange(source_ip, query_ip + "/" + query_ip_mask));
+    }
+
+    @Test
+    public void ipv6Mask() throws UnknownHostException {
+        String query_ip = "234e:0:4567::3d";
+        int query_ip_mask = 16;
+        String source_ip = "234e:3:4567:0::3a";
+
+        System.out.println("判断：" + isInRange(source_ip, query_ip, query_ip_mask));
+
+        InetAddress query_host = InetAddress.getByName(query_ip);
+        InetAddress source_host = InetAddress.getByName(source_ip);
+        System.out.println(query_host);
+        System.out.println(source_host);
+//        BitSet qbs = byteArray2BitSet(query_host.getAddress());
+//        BitSet sbs = byteArray2BitSet(source_host.getAddress());
+        byte[] qbsArray = query_host.getAddress();
+        System.out.println("query_host第一个byte：" + byteToBit(qbsArray[0]));
+        ByteBuffer bb = ByteBuffer.wrap(qbsArray);
+        System.out.println("ByteBuffer bb第一个byte：" + byteToBit(bb.get(0)));
+        BitSet qbs = BitSet.valueOf(query_host.getAddress());
+        BitSet sbs = BitSet.valueOf(source_host.getAddress());
+        BitSet qbs1 = byteArray2BitSet(query_host.getAddress());
+        BitSet qbs2 = bytesToBitSet(query_host.getAddress());
+        System.out.println("qbs1：" + qbs1 + "，" + qbs1.size());
+        System.out.println("qbs2：" + qbs2 + "，" + qbs2.size());
+        System.out.println("qbs：" + qbs + "，" + qbs.size());
+        System.out.println("qbs1.equals(qbs)：" + qbs1.equals(qbs));
+        System.out.print("qbs1第一个byte：");
+        for (int i = 0; i < 8; i++) {
+            System.out.print(qbs1.get(i) ? 1 : 0);
+        }
+        System.out.println();
+        System.out.print("qbs2第一个byte：");
+        for (int i = 0; i < 8; i++) {
+            System.out.print(qbs2.get(i) ? 1 : 0);
+        }
+        System.out.println();
+        System.out.print("qbs第一个byte：");
+        for (int i = 0; i < 8; i++) {
+            System.out.print(qbs.get(i) ? 1 : 0);
+        }
+        System.out.println();
+
+        System.out.println("sbs：" + sbs + "，" + sbs.size());
+        BitSet mask = new BitSet();
+        for (int i = 0; i < query_ip_mask; i++) {
+            mask.set(i, true);
+        }
+        qbs.and(mask);
+        sbs.and(mask);
+        System.out.println("qbs.equals(sbs)：" + qbs.equals(sbs));
+
+        for (int i = 0; i < 64; i++) {
+            System.out.print(1);
+        }
+        for (int i = 0; i < 64; i++) {
+            System.out.print(0);
         }
     }
 }
